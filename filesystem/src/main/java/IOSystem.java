8487171b5f7ff8d99e0badb5ce53c104b794b2fa
trilloner <bo_10@ukr.net>
2021-04-06 00:12:12 +0300

    Update IOSystem.java

--- filesystem/src/main/java/IOSystem.java
+++ filesystem/src/main/java/IOSystem.java
@@ -1,21 +1,25 @@
 public class IOSystem {
     private final int LENGTH = 64;
-    private final int BLOCK = 64;
-    private char[][] ldisk;
+    private final int BLOCK_SIZE = 64;
+    private final byte[][] ldisk;
 
     public IOSystem() {
-        this.ldisk = new char[LENGTH][BLOCK];
+        this.ldisk = new byte[LENGTH][BLOCK_SIZE];
     }
 
     public IOSystem(int l, int b) {
-        this.ldisk = new char[l][b];
+        this.ldisk = new byte[l][b];
     }
 
-    public void read_block(int i, char[] p) {
-        System.arraycopy(this.ldisk[i], 0, p, 0, BLOCK);
+    public void readBlock(int i, byte[] p) {
+        for (int j = 0; j < p.length; j++) {
+            p[j] = this.ldisk[i][j];
+        }
     }
 
-    public void write_block(int i, char[] p) {
-        System.arraycopy(p, 0, this.ldisk[i], 0, BLOCK);
+    public void writeBlock(int i, byte[] p) {
+        for (int j = 0; j < p.length; j++) {
+            this.ldisk[i][j] = p[j];
+        }
     }
 }
